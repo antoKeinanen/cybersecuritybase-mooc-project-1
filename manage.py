0835672983acf7eeb053805a6b5cf928c66f9710
AntoKeinanen <anto.keinanen@gmail.com>
2025-03-01 10:29:43 +0200

    chore: restructure project

--- manage.py
+++ manage.py
@@ -3,10 +3,11 @@
 import os
 import sys
 
+SERVER_DIR = 'project'
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', SERVER_DIR + '.config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
---
+++ readme.md
@@ -0,0 +1,2 @@
+https://cybersecuritybase.mooc.fi/module-3.1
+https://web.archive.org/web/20250301081009/https://cybersecuritybase.mooc.fi/module-3.1
